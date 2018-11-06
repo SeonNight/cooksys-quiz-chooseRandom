448bf839486710a1acd27030598ace11d1c2c2ca
Seong <kirakim96@hotmail.com>
2018-11-06 14:54:27 -0600

    Added comments; cleaned code

--- src/lib.js
+++ src/lib.js
@@ -1,66 +1,48 @@
 export const chooseRandom = (array, numItems) => {
-    console.log("--------------------------")
-    console.log("--Choosing Random--")
-    console.log("Original Array: ")
-    console.log(array)
-    console.log("Given Length: ")
-    console.log(numItems)
-
-    let randomIndices
-    let newArray
+    //If array is undefined return an empty array
     if(array === undefined) {
-        console.log("Undefined Array")
-        console.log(new Array())
-        return new Array()
+        return []
     }
+    //If the number given is invalid get a new random number
     if(numItems === undefined || numItems < 1 || numItems > array.length) {
         //numItems should be 1 to arraylength
         numItems = Math.floor(Math.random() * array.length) + 1;
         if(array.length == 0) {
-            numItems = 0;
+            numItems = 0
         }
     }
-    console.log("Length of Array: ")
-    console.log(array.length)
-    console.log("Length of random Array: ")
-    console.log(numItems)
-    newArray = new Array(numItems)
-
-    if(array.length == 1) {
-        newArray[0] = array[0]
-        console.log("Array had the length of 1: ")
-        console.log(newArray.length)
-        console.log(newArray)
-        //return newArray
-        return array
-    } else if (array.length == 0) {
-        console.log("Array had the length of 0: ")
-        console.log(newArray.length)
-        console.log(newArray)
-        //return newArray
+    //If the array length is less than 2 (1 or 0) just return
+    //  the given array
+    if(array.length < 2) {
         return array
     }
 
-    randomIndices = new Array(numItems)
+    //Array to store the random array
+    let newArray = new Array(numItems)
+    //Array to store random index numbers
+    let randomIndices = new Array(numItems)
 
-    console.log("Randomization")
+    //Get the random indexes
     for(let i = 0; i < randomIndices.length; i++) {
+        //get a random number
         let index = Math.floor(Math.random() * array.length)
+        //If there is a repeat index get a new number
         while(randomIndices.includes(index)) {
-            index = Math.floor(Math.random() * array.length)
+            //Just increment
+            index++
+            if(index >= array.length) {
+                index = 0
+            }
         }
-        console.log("--" + index)
+        //store the index
         randomIndices[i] = index
     }
-
+    
+    //Put the values into the new random array using the random indices
     for(let i = 0; i < randomIndices.length; i++) {
         newArray[i] = array[randomIndices[i]]
     }
 
-    console.log("New Random Array: ")
-    console.log(newArray)
-
-    console.log("--------------------------")
-    console.log()
+    //return the new random array
     return newArray
 }
