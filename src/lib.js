d2bd35703451838eba33abb9a6697156935fb88e
Seong <kirakim96@hotmail.com>
2018-11-07 15:09:19 -0600

    A one lined mess

--- src/lib.js
+++ src/lib.js
@@ -4,24 +4,9 @@ export const chooseRandom = (array, numItems) => {
         [] //If array is undefined return an empty array
         : (array.length < 2) ?
             array //If array has the length of 0 or 1 just return the array
-            : randomShuffle(array, //If array has at least 1 elements Shuffle the array
-                (numItems === undefined || numItems < 1 || numItems > array.length) ?
+            : array.slice().map((cur, ind, curArray) => { //Create a random array with given length
+                return curArray.splice(Math.floor(Math.random() * (array.length - ind)) + ind,1,curArray[ind])[0]
+                }).splice(0,(numItems === undefined || numItems < 1 || numItems > array.length) ?
                     Math.floor(Math.random() * array.length) + 1 //If given number is invalid return a number between 1 and array length
-                    : numItems) //if valid return given number
-
-    //Given array and number (length)
-    function randomShuffle(array, n) {
-        //Get length of array
-        let pos = array.length
-        //Get a copy of array
-        let copyArray = array.slice()
-
-        //Go through and shuffle moving shuffled values to the back
-        for(let i = array.length - 1; array.length - i <= n; i--) {
-            copyArray[i] = copyArray.splice(Math.floor(Math.random() * pos--),1,copyArray[i])[0]
-        }
-
-        //return shuffled variables of the correct length
-        return copyArray.splice(copyArray.length-n,n)
-    }
+                    : numItems)
 }
